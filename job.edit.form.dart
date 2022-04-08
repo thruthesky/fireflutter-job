0ec905c61ddb73fe854e5de265e9c6ce56c4dbf3
JaeHo Song <thruthesky@gmail.com>
2022-04-08 17:07:17 +0900

    update job

---
+++ job.edit.form.dart
@@ -0,0 +1,149 @@
+import 'package:fe/service/app.service.dart';
+import 'package:fireflutter/fireflutter.dart';
+import 'package:flutter/material.dart';
+
+class JobEditForm extends StatefulWidget {
+  const JobEditForm({
+    Key? key,
+    required this.onError,
+  }) : super(key: key);
+
+  final Function onError;
+
+  @override
+  State<JobEditForm> createState() => _JobEditFormState();
+}
+
+class _JobEditFormState extends State<JobEditForm> {
+  final companyName = TextEditingController();
+  final phoneNumber = TextEditingController();
+  final mobileNumber = TextEditingController();
+  final email = TextEditingController();
+  final jobCategory = TextEditingController();
+  final workingHours = TextEditingController();
+  final detailAddress = TextEditingController();
+  final aboutUs = TextEditingController();
+  final numberOfHiring = TextEditingController();
+  final jobDescription = TextEditingController();
+  final requirement = TextEditingController();
+  final duty = TextEditingController();
+  final salary = TextEditingController();
+  final benefit = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Create a job opening'),
+        TextField(
+          controller: companyName,
+          decoration: InputDecoration(
+            labelText: "Company name",
+          ),
+        ),
+        TextField(
+          controller: mobileNumber,
+          decoration: InputDecoration(
+            labelText: "Mobile phone number",
+          ),
+        ),
+        TextField(
+          controller: phoneNumber,
+          decoration: InputDecoration(
+            labelText: "Office phone number",
+          ),
+        ),
+        TextField(
+          controller: email,
+          decoration: InputDecoration(
+            labelText: "Email address",
+          ),
+        ),
+        ElevatedButton(
+            onPressed: () {
+              AppService.instance.inputAddress(context);
+            },
+            child: Text('Input address')),
+        TextField(
+          controller: jobCategory,
+          decoration: InputDecoration(
+            labelText: "Job category(industry) - @todo select box",
+          ),
+        ),
+        TextField(
+          controller: aboutUs,
+          decoration: InputDecoration(
+            labelText: "About us",
+          ),
+        ),
+        TextField(
+          controller: numberOfHiring,
+          decoration: InputDecoration(
+            labelText: "Number of hiring",
+          ),
+        ),
+        TextField(
+          controller: workingHours,
+          decoration: InputDecoration(
+            labelText: "Working hours",
+          ),
+        ),
+        TextField(
+          controller: jobDescription,
+          decoration: InputDecoration(
+            labelText: "Job description(details of what workers will do)",
+          ),
+        ),
+        TextField(
+          controller: requirement,
+          decoration: InputDecoration(
+            labelText: "Requirements and qualifications",
+          ),
+        ),
+        TextField(
+          controller: duty,
+          decoration: InputDecoration(
+            labelText: "Duties and responsibilities",
+          ),
+        ),
+        TextField(
+          controller: salary,
+          decoration: InputDecoration(
+            labelText: "Salary",
+          ),
+        ),
+        TextField(
+          controller: benefit,
+          decoration: InputDecoration(
+            labelText: "benefits(free meals, dormitory, transporation, etc)",
+          ),
+        ),
+        Divider(),
+        ElevatedButton(
+          onPressed: () async {
+            try {
+              await PostApi.instance.create(category: 'job_openings', extra: {
+                'companyName': companyName.text,
+                'phoneNumber': phoneNumber.text,
+                'mobileNumber': mobileNumber.text,
+                'email': email.text,
+                'jobCategory': jobCategory.text,
+                'workingHours': workingHours.text,
+                'detailAddress': detailAddress.text,
+                'aboutUs': aboutUs.text,
+                'numberOfHiring': numberOfHiring.text,
+                'jobDescription': jobDescription.text,
+                'requirement': requirement.text,
+                'duty': duty.text,
+                'salary': salary.text,
+                'benefit': benefit.text,
+              });
+            } catch (e) {
+              widget.onError(e);
+            }
+          },
+          child: Text('Submit'),
+        )
+      ],
+    );
+  }
+}
